9a50dec8a1009350dd57e2a85143978169a86ef6
Denver Coneybeare <dconeybe@google.com>
2024-06-21 20:01:12 +0000

    queryShouldRetryOnUnauthenticated added

--- firebase-dataconnect/src/androidTest/kotlin/com/google/firebase/dataconnect/AuthIntegrationTest.kt
+++ firebase-dataconnect/src/androidTest/kotlin/com/google/firebase/dataconnect/AuthIntegrationTest.kt
@@ -22,20 +22,38 @@ import com.google.common.truth.Truth.assertWithMessage
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.dataconnect.testutil.DataConnectBackend
 import com.google.firebase.dataconnect.testutil.DataConnectIntegrationTestBase
+import com.google.firebase.dataconnect.testutil.InProcessDataConnectGrpcServer
 import com.google.firebase.dataconnect.testutil.assertThrows
+import com.google.firebase.dataconnect.testutil.newInstance
+import com.google.firebase.dataconnect.testutil.operationName
 import com.google.firebase.dataconnect.testutil.schemas.PersonSchema
 import com.google.firebase.dataconnect.testutil.schemas.PersonSchema.GetPersonAuthQuery
 import com.google.firebase.dataconnect.testutil.schemas.randomPersonId
+import com.google.firebase.dataconnect.util.buildStructProto
 import com.google.firebase.util.nextAlphanumericString
+import google.firebase.dataconnect.proto.executeQueryResponse
 import io.grpc.Status
+import io.kotest.assertions.asClue
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.RandomSource
+import io.kotest.property.arbitrary.next
 import kotlin.random.Random
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AuthIntegrationTest : DataConnectIntegrationTestBase() {
+
+  private val key = "e6w33rw36t"
+  private val rs = RandomSource.default()
+
+  @get:Rule val inProcessDataConnectGrpcServer = InProcessDataConnectGrpcServer()
 
   private val personSchema by lazy { PersonSchema(dataConnectFactory) }
 
@@ -92,6 +110,25 @@ class AuthIntegrationTest : DataConnectIntegrationTestBase() {
     assertThat(exception.status.code).isEqualTo(Status.UNAUTHENTICATED.code)
   }
 
+  @Test
+  fun queryShouldRetryOnUnauthenticated() = runTest {
+    val responseData = buildStructProto { put("foo", key) }
+    val executeQueryResponse = executeQueryResponse { data = responseData }
+    val grpcServer =
+      inProcessDataConnectGrpcServer.newInstance(
+        errors = listOf(Status.UNAUTHENTICATED),
+        executeQueryResponse = executeQueryResponse
+      )
+    val dataConnect = dataConnectFactory.newInstance(grpcServer)
+    val operationName = Arb.operationName(key).next(rs)
+    val queryRef =
+      dataConnect.query(operationName, Unit, serializer<TestData>(), serializer<Unit>())
+
+    val actualResponse = queryRef.execute()
+
+    actualResponse.asClue { it.data shouldBe TestData(key) }
+  }
+
   private suspend fun signIn() {
     val authResult = auth.run { signInAnonymously().await() }
     assertWithMessage("authResult.user").that(authResult.user).isNotNull()
@@ -100,4 +137,6 @@ class AuthIntegrationTest : DataConnectIntegrationTestBase() {
   private fun signOut() {
     auth.run { signOut() }
   }
+
+  @Serializable data class TestData(val foo: String)
 }
